f9c21afcc37b7d4d07cb3ff48873a2dd810fdf82
L4ksa <155923207+L4ksa@users.noreply.github.com>
2024-06-13 11:47:03 +0800

    ContactSection.jsx

--- src/data/ContactSection.jsx
+++ src/data/ContactSection.jsx
@@ -8,7 +8,7 @@ export const contactSection = [
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, culpa!</p>`
     },
     {
-        content: `<h3>Contact</h3>
+        content: `<h3>Address</h3>
                     <p>Lot 8992, Jalan Mendu, Kampung Simpang Tiga, 93200 Kuching, Sarawak</p>`
     },
     {
